eeb928fc8da8b98f1cf54672c4d899c69ec05f80
Chandan Akiti <chandanakiti@gmail.com>
2021-01-04 21:20:30 -0500

    Update proto_network.py

--- models/proto_network.py
+++ models/proto_network.py
@@ -1,12 +1,17 @@
 import coloredlogs
 import logging
 import os
+import pdb
+import random
 import torch
+from tqdm import tqdm
 import numpy as np
 from datetime import datetime
 from torch.utils.tensorboard import SummaryWriter
 
 from models.seq_proto import SeqPrototypicalNetwork
+from models.seq_proto_online import SeqPrototypicalOnlineNetwork
+from models import utils
 
 logger = logging.getLogger('ProtoLearningLog')
 coloredlogs.install(logger=logger, level='DEBUG', fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -27,6 +32,8 @@ class PrototypicalNetwork:
 
         if 'seq' in config['meta_model']:
             self.proto_model = SeqPrototypicalNetwork(config)
+        if 'online' in config['meta_model']:
+            self.proto_model = SeqPrototypicalOnlineNetwork(config)
 
         logger.info('Prototypical network instantiated')
 
@@ -34,11 +41,17 @@ class PrototypicalNetwork:
         best_loss = float('inf')
         best_f1 = 0
         patience = 0
+        
+        # model_path = os.path.join(self.base_path, 'saved_models', 'Supervised-stable.h5')
+        # self.proto_model.learner.load_state_dict(torch.load(model_path))
+        
         model_path = os.path.join(self.base_path, 'saved_models', 'ProtoNet-{}.h5'.format(self.stamp))
         logger.info('Model name: ProtoNet-{}.h5'.format(self.stamp))
+        
         for epoch in range(self.meta_epochs):
             logger.info('Starting epoch {}/{}'.format(epoch + 1, self.meta_epochs))
-            losses, accuracies, precisions, recalls, f1s = self.proto_model(train_episodes, self.updates)
+            losses, accuracies, precisions, recalls, f1s, _, _ = self.proto_model(train_episodes, 
+                                                                                  self.updates)
             avg_loss = np.mean(losses)
             avg_accuracy = np.mean(accuracies)
             avg_precision = np.mean(precisions)
@@ -52,17 +65,20 @@ class PrototypicalNetwork:
             self.tensorboard_writer.add_scalar('Loss/train', avg_loss, global_step=epoch + 1)
             self.tensorboard_writer.add_scalar('F1/train', avg_f1, global_step=epoch + 1)
 
-            losses, accuracies, precisions, recalls, f1s = self.proto_model(val_episodes, self.updates, testing=True)
+            losses, _, _, _, _, val_predictions, val_labels = self.proto_model(random.sample(val_episodes, 200), 
+                                                                               self.updates, 
+                                                                               testing=True)
 
+            accuracy, precision, recall, avg_f1 = utils.calculate_seqeval_metrics(val_predictions,
+                                                                                    val_labels, 
+                                                                                    tags=val_episodes[0].tags, 
+                                                                                    binary=False)
+            
             avg_loss = np.mean(losses)
-            avg_accuracy = np.mean(accuracies)
-            avg_precision = np.mean(precisions)
-            avg_recall = np.mean(recalls)
-            avg_f1 = np.mean(f1s)
 
             logger.info('Meta val epoch {}: Avg loss = {:.5f}, avg accuracy = {:.5f}, avg precision = {:.5f}, '
-                        'avg recall = {:.5f}, avg F1 score = {:.5f}'.format(epoch + 1, avg_loss, avg_accuracy,
-                                                                            avg_precision, avg_recall, avg_f1))
+                        'avg recall = {:.5f}, avg F1 score = {:.5f}'.format(epoch + 1, avg_loss, accuracy,
+                                                                            precision, recall, avg_f1))
 
             self.tensorboard_writer.add_scalar('Loss/val', avg_loss, global_step=epoch + 1)
             self.tensorboard_writer.add_scalar('F1/val', avg_f1, global_step=epoch + 1)
@@ -71,8 +87,8 @@ class PrototypicalNetwork:
                 patience = 0
                 best_loss = avg_loss
                 best_f1 = avg_f1
-                torch.save(self.proto_model.learner.state_dict(), model_path)
                 logger.info('Saving the model since the F1 improved')
+                torch.save(self.proto_model.learner.state_dict(), model_path)
                 logger.info('')
             else:
                 patience += 1
@@ -82,21 +98,24 @@ class PrototypicalNetwork:
                     break
 
             # Log params and grads into tensorboard
-#             for name, param in self.proto_model.named_parameters():
-#                 if param.requires_grad and param.grad is not None:
-#                     self.tensorboard_writer.add_histogram('Params/' + name, param.data.view(-1),
-#                                                      global_step=epoch + 1)
-#                     self.tensorboard_writer.add_histogram('Grads/' + name, param.grad.data.view(-1),
-#                                                      global_step=epoch + 1)
+            for name, param in self.proto_model.named_parameters():
+                if param.requires_grad and param.grad is not None:
+                    self.tensorboard_writer.add_histogram('Params/' + name, param.data.view(-1),
+                                                     global_step=epoch + 1)
+                    self.tensorboard_writer.add_histogram('Grads/' + name, param.grad.data.view(-1),
+                                                     global_step=epoch + 1)
 
-        self.proto_model.learner.load_state_dict(torch.load(model_path))
+        # self.proto_model.learner.load_state_dict(torch.load(model_path))
         return best_f1
 
-    def testing(self, test_episodes):
+    def episodic_testing(self, test_episodes):
         logger.info('---------- Proto testing starts here ----------')
+        # model_path = os.path.join(self.base_path, 'saved_models', 'ProtoNet-{}.h5'.format(self.stamp))
+        model_path = os.path.join(self.base_path, 'saved_models', 'Supervised-stable.h5')
+        self.proto_model.learner.load_state_dict(torch.load(model_path))
         episode_accuracies, episode_precisions, episode_recalls, episode_f1s = [], [], [], []
         for episode in test_episodes:
-            _, accuracy, precision, recall, f1_score = self.proto_model([episode], updates=self.updates, testing=True)
+            _, accuracy, precision, recall, f1_score, _, _ = self.proto_model([episode], updates=self.updates, testing=True)
             accuracy, precision, recall, f1_score = accuracy[0], precision[0], recall[0], f1_score[0]
 
             episode_accuracies.append(accuracy)
@@ -110,3 +129,29 @@ class PrototypicalNetwork:
                                                np.mean(episode_recalls),
                                                np.mean(episode_f1s)))
         return np.mean(episode_f1s)
+
+    def testing(self, test_episodes):
+        logger.info('---------- Proto testing starts here ----------')
+        # model_path = os.path.join(self.base_path, 'saved_models', 'ProtoNet-{}.h5'.format(self.stamp))
+#         model_path = os.path.join(self.base_path, 'saved_models', 'Supervised-stable.h5')
+#         self.proto_model.learner.load_state_dict(torch.load(model_path))
+        
+        all_predictions, all_labels = [], []
+        
+        for episode in tqdm(test_episodes):
+            _, _, _, _, _, predictions, labels = self.proto_model([episode], updates=self.updates, testing=True)
+
+            all_predictions.extend(predictions)
+            all_labels.extend(labels)
+        
+        # pdb.set_trace()
+        
+        accuracy, precision, recall, f1_score = utils.calculate_seqeval_metrics(all_predictions,
+                                                                    all_labels, tags=episode.tags, binary=False)
+
+        logger.info('Avg meta-testing metrics: Accuracy = {:.5f}, precision = {:.5f}, recall = {:.5f}, '
+                    'F1 score = {:.5f}'.format(accuracy,
+                                               precision,
+                                               recall,
+                                               f1_score))
+        return f1_score
